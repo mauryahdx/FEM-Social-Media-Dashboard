b00a4013f1236e4def5b47fdc27d65edc53d8095
Surya Prakash Mourya <suryamourya@iiitdmj.ac.in>
2021-12-22 00:01:35 +0530

    made toggle functional with javasript and localstorage

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,51 @@
-console.log("Sudama")
+const dark = document.querySelector('#dark');
+const light = document.querySelector('#light');
+const toggle = document.querySelector('.toggle');
+const body = document.querySelector('body');
+
+function setDarkTheme(){
+    body.classList.remove('light');
+    body.classList.add('dark');
+    localStorage.setItem("colormode", "dark");
+    
+}
+
+function setLightTheme(){
+    body.classList.remove('dark');
+    body.classList.add('light');
+    localStorage.setItem("colormode", "light");
+   
+}
+
+
+
+if(localStorage.getItem("colormode")){
+   const theme = localStorage.getItem("colormode").toString()
+   console.log(theme)
+   if(theme == 'light')
+   {
+       
+       setLightTheme()
+       light.checked=true;
+   } else {
+       
+       setDarkTheme();
+       dark.checked=true;
+   }
+  // theme.checked = true;
+}
+
+toggle.addEventListener('click', () => {
+if (dark.checked){
+    setDarkTheme()
+    
+}
+
+if (light.checked){
+    setLightTheme()
+    
+}
+
+})
+
+
